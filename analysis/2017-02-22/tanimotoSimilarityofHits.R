246da394d63e76ca550561a73b4214299f90180c
allaway <robert.allaway@sagebase.org>
2017-02-23 11:02:25 -0800

    Revert "plotting DE genes and tanimoto scores"
    This reverts commit 10eba48226783825f84c8baf2439275d9d70e274.

--- analysis/2017-02-22/tanimotoSimilarityofHits.R
+++ analysis/2017-02-22/tanimotoSimilarityofHits.R
@@ -6,10 +6,9 @@ library(rcdk)
 synapseLogin()
 library(pheatmap)
 
-###NF1 TCGA DEGene Drug Hits 
 x<-synGet("syn8295452")@filePath
 data<-read.table(x)
-sigs<-filter(data, Hypergeo_pval<=0.05)
+sigs<-filter(data, pval_BHadj<=0.1)
 sig.data<-select(sigs, Structure_ID, Original_molecule_SMILES, Supplier_Data_1,Supplier_Data_2,Supplier_Data_3)
 sig.data<-distinct(sig.data)
 
@@ -23,30 +22,8 @@ fp.sim <- fp.sim.matrix(fps, method = "tanimoto")
 rownames(fp.sim) <- sig.data$Structure_ID
 colnames(fp.sim) <- sig.data$Structure_ID
 fp.dist <- 1- fp.sim
-pheatmap(fp.sim, border_color = NA, color = magma(n = 10000))
+pheatmap(fp.dist)
 
 clust<-hclust(as.dist(fp.dist))
 plot(clust)
 
-
-###NF1 TCGA Comutant Hits
-x<-synGet("syn8292948")@filePath
-data<-read.table(x)
-sigs<-filter(data, Hypergeo_pval<=0.0005)
-sig.data<-select(sigs, Structure_ID, Original_molecule_SMILES, Supplier_Data_1,Supplier_Data_2,Supplier_Data_3)
-sig.data<-distinct(sig.data)
-
-mol<-parse.smiles(as.character(sig.data$Original_molecule_SMILES))
-
-#write.molecules(mol, filename = 'NF1_TCGA_drugs.sdf')
-#view.molecule.2d(mol)
-
-fps <- lapply(mol, get.fingerprint, type="extended")
-fp.sim <- fp.sim.matrix(fps, method = "tanimoto")
-rownames(fp.sim) <- sig.data$Structure_ID
-colnames(fp.sim) <- sig.data$Structure_ID
-fp.dist <- 1- fp.sim
-pheatmap(fp.sim, border_color = NA, color = magma(n = 10000))
-
-clust<-hclust(as.dist(fp.dist))
-plot(clust)
