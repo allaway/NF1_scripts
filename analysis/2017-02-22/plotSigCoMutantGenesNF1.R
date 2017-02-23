f6d86e79b4ef15bf6e4680841c651e2eace3abb4
allaway <robert.allaway@sagebase.org>
2017-02-23 11:02:57 -0800

    Revert "Revert "plotting DE genes and tanimoto scores""
    This reverts commit 246da394d63e76ca550561a73b4214299f90180c.

--- analysis/2017-02-22/plotSigCoMutantGenesNF1.R
+++ analysis/2017-02-22/plotSigCoMutantGenesNF1.R
@@ -5,7 +5,7 @@ library(synapseClient)
 library(ggplot2)
 synapseLogin()
 
-this.file<-"https://raw.githubusercontent.com/allaway/NF1_scripts/master/bin/SignificantlyCoMutatedGenes.R"
+this.file<-"https://raw.githubusercontent.com/allaway/NF1_scripts/master/analysis/2017-02-22/plotSigCoMutantGenesNF1.R"
 
 patients<-read.table("../../bin/TCGA_NF1_patient_status.txt")
 patients<-filter(patients, mutation==1)
@@ -25,26 +25,35 @@ library(viridis)
 files <-
   synQuery("SELECT * from file WHERE parentId=='syn8303919'")$file.id
 
-i <- "syn8303923"
-
 for(i in files){
     data<-read.table(synGet(i)@filePath, sep = "\t", header = TRUE)
     cancer<-synGet(i)@fileHandle$fileName
     cancer<-sub("_NF1_comut_TCGA.txt", "", cancer)
-    bar<-filter(data, p_value <= 0.01)
+    bar<-filter(data, p_value <= 0.1)
     list<-bar$gene
     dat<-filter(mutations[[cancer]], gene %in% list)
     dat<-select(dat, gene, one_of(mutpts))
+    
+    if(NCOL(dat[,-1])>1 & NROW(dat[,-1])>1){
     dat$sums<-rowSums(dat[,-1])
     dat<-select(dat, gene, sums)
     dat$gene <- factor(dat$gene, levels = dat$gene[order(desc(dat$sums))])
+    
+    if(length(dat$gene)>30){
+      dat<-arrange(dat, desc(sums))
+      dat<-slice(dat, 1:30)
+    }
     virid <- rev(viridis(n = nrow(dat)))
     ggplot(data = dat, aes(x=gene, y=sums)) +
       geom_bar(stat = "identity", color = "darkgray", fill = virid) +
       theme(axis.text.x = element_text(angle = 50, hjust = 1)) +
-      labs(title = paste("NF1 co-mutated genes in ", cancer, " (corr. p<0.01)", sep = ""), x = "Gene", y = "Comutation Frequency") +
+      labs(title = paste("NF1 co-mutated genes in ", cancer, " (corr. p<0.1, top 30)", sep = ""), x = "Gene", y = "Comutation Frequency") +
       theme(plot.title=element_text(hjust=0.5))
     ggsave(paste("sig_NF1_co_mutant_genes_in_",cancer,"_TCGA.png", sep = ""), plot = last_plot(), width = 7, height = 5.5) 
     synStore(File(paste("sig_NF1_co_mutant_genes_in_",cancer,"_TCGA.png", sep = ""),parentId = "syn8304189"), used = i, executed = this.file) 
+    } else {
+      print("no sig comuts") 
+    }
+    
     }
 
